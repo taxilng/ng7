13ab7d1c8a4dbf9942f76320f6d91a6fd12ffd72
taxilng <769967440@qq.com>
2018-12-28 16:40:48 +0800

    demo搞定，一脸懵逼

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -20,5 +20,22 @@ export class HeroDetailComponent implements OnInit {
     private location: Location
   ) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getHero()
+  }
+  
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+  
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
